42af7e027e7fa2d1fb771cdc84717a389ccfc2d9
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-10-25 18:50:43 +0200

    Implementiere Aufgabe 1.3

---
+++ uebung1_3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from math import sin,cos,tan,log
+
+def cot(x):
+    return 1/tan(x)
+
+def f(x):
+    return x**2/ sin(x)
+
+def df(x):
+    return (2*x - x**2 * cot(x))/sin(x)
+
+hs = [ 10**(-i) for i in range(1,16) ]
+
+def zentralDiff(f, x, h):
+    return (f(x+h) - f(x-h))/(2*h)
+
+def fehlerDiff(f, df, x, h):
+    return abs(zentralDiff(f, x, h) - df(x))
+
+for h in hs:
+    print("Fehler fuer h = 10**%i" % log(h,10),end="\t")
+    print(fehlerDiff(f,df,1,h))
