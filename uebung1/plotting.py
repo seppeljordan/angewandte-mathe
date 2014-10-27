9381024625dc7d979d138947c12a941a0a886f44
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-10-27 16:39:03 +0100

    Ergaenze plotting.py um Kommentar

--- uebung1/plotting.py
+++ uebung1/plotting.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 from uebung1_3 import *
 
 def plot_uebung1_3():
+    """Plotte die Werte die in der Aufgabe geforderdert sind als Graph"""
+    # fehlers soll die liste der fehler sein.
     fehlers = []
+    # erstelle die liste der fehler zum zugehoerigen h
     for x in hs:
         fehlers.append(fehlerDiff(f,df,1,x))
+    # erstelle plot fuer die h's und die fehler
     plt.plot(hs,fehlers)
+    # lege achsenminima und maxima fest
     plt.axis([min(hs),max(hs),min(fehlers),max(fehlers)])
+    # skaliere die axen logrithmisch
     plt.yscale('log')
     plt.xscale('log')
+    # zeige den graphen
     plt.show()
